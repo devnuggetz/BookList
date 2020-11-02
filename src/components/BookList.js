1100c4af7c0ec28bf5d9496fbeaea66ed5fe0980
shuvamk <kshuvam564@gmail.com>
2020-11-01 17:58:02 -0800

    Replaced Class component with functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../ contexts/ThemeContext'
 
-export class BookList extends Component {
-    static contextType= ThemeContext;
-    render() {
-        const {isLightTheme, light, dark}= this.context;
-        const theme= isLightTheme?light:dark;
-        return (
-            <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
-                <ul>
-                    <li style={{background: theme.ui}}>Harry Potter</li>
-                    <li style={{background: theme.ui}}>Ranodm Book</li>
-                    <li style={{background: theme.ui}}>XYZ book</li>
-                </ul>
-            </div>
-        )
-    }
+const BookList = () => {
+    const {isLightTheme, light, dark}=useContext(ThemeContext);
+    const theme= isLightTheme?light:dark;
+    return ( 
+        <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+        <ul>
+            <li style={{background: theme.ui}}>Harry Potter</li>
+            <li style={{background: theme.ui}}>Ranodm Book</li>
+            <li style={{background: theme.ui}}>XYZ book</li>
+        </ul>
+    </div>
+     );
 }
-
-export default BookList
+ 
+export default BookList;
